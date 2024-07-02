7f911948a3f04fe450e84b53b95e83889610f401
hadi-razal <hadhirasal22@gmail.com>
2024-07-02 22:48:39 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishlistItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('To Learn MySQL', true),
+    new WishItem('To Learn SpringBoot', false)
+  ];
+
   title = 'wishlist';
 }
