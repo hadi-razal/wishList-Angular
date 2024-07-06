092b432a033580bc9dbe17cc447f8ccadad3f33c
hadi-razal <hadhirasal22@gmail.com>
2024-07-06 23:56:42 +0530

    working on add wish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,33 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishlistItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   items: WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('To Learn MySQL', true),
-    new WishItem('To Learn SpringBoot', false)
+    // new WishItem('To Learn Angular'),
+    // new WishItem('To Learn MySQL', true),
+    // new WishItem('To Learn SpringBoot', false)
   ];
 
-
-  newWishItem = '';
-
+  newWishText = '';
 
   title = 'wishlist';
 
-
-
-
-  addNewWish(text: string) {
-    new WishItem(text, false)
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = ''
   }
-
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
-    console.log(item)
-    alert('Toggle Item');
+    console.log(item);
   }
 }
