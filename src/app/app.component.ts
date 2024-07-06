53e6b7d78ef79622badcf26a5fab8a5c90cced26
hadi-razal <hadhirasal22@gmail.com>
2024-07-06 14:47:27 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,23 @@ export class AppComponent {
     new WishItem('To Learn SpringBoot', false)
   ];
 
+
+  newWishItem = '';
+
+
   title = 'wishlist';
+
+
+
+
+  addNewWish(text: string) {
+    new WishItem(text, false)
+  }
+
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item)
+    alert('Toggle Item');
+  }
 }
